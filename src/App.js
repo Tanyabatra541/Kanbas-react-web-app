fadd7a2f76c9ab469ab119dd9485d992be84c98f
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2023-10-19 23:49:59 -0400

    Labs a3

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import Labs from "./Labs";
 import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
       <div>
-        {/* <h1>React Labs</h1> */}
-        {/* <Link to="/Hello">Hello World</Link><br />
+        <h1>React Labs</h1> 
+        <Link to="/Hello">Hello World</Link><br />
         <Link to="/Labs">Labs</Link><br />
-        <Link to="/Kanbas">Kanbas</Link><br /> */}
+        <Link to="/Kanbas">Kanbas</Link><br />
         <Routes>
-          <Route path="/" element={<Navigate to="Kanbas" />}></Route>
+          {/* <Route path="/" element={<Navigate to="Ka />}></Route> */}
           <Route path="/Hello" element={<HelloWorld />}></Route>
           <Route path="/Labs/*" element={<Labs />}></Route>
           <Route path="/Kanbas/*" element={<Kanbas />}></Route>
