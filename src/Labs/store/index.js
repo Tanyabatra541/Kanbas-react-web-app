04d28cc27ee27d32b313df909001491f7e478dea
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2023-11-02 23:46:48 -0400

    updated

--- src/Labs/store/index.js
+++ src/Labs/store/index.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../a4/ReduxExamples/HelloRedux/helloReducer";
 import counterReducer from "../a4/ReduxExamples/CounterRedux/counterReducer";
 import addReducer from "../a4/ReduxExamples/AddRedux/addReducer";
-import todosReducer from "/Users/tanyabatra/Desktop/2023/Fall 2023/Web Development/Kanbas/Kanbas-react-web-app/src/Labs/a4/todos/todosReducer.js";
+import todosReducer from "../a4/todos/todosReducer";
 const store = configureStore({
   reducer: {
     helloReducer,
