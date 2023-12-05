335b6b6f29ddb593707b63ff75ade3ea3b7843fe
Tanya Batra <tanyabatra@Tanyas-MacBook-Pro.local>
2023-12-04 21:33:31 -0500

    account id added

--- src/project/users/account.js
+++ src/project/users/account.js
@@ -1,9 +1,13 @@
 import * as client from "./client";
 import { useState, useEffect } from "react";
 import { useNavigate, Link, useParams } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 function Account() {
-  const { id } = useParams();
+  // const { id } = useParams();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const id = params.get('id');
   const [account, setAccount] = useState(null);
   const navigate = useNavigate();
 
--- src/project/users/signin.js
+++ src/project/users/signin.js
@@ -3,12 +3,17 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Signin() {
-  const [credentials, setCredentials] = useState({ username: "", password: "" });
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
   const navigate = useNavigate();
 
   const signin = async () => {
-    await client.signin(credentials);
-    navigate("/project/account");
+    const response = await client.signin(credentials);
+    // navigate("/project/account");
+    const userId = String(response._id);
+    navigate(`/project/account?id=${userId}`);
   };
 
   return (
@@ -28,7 +33,12 @@ function Signin() {
                     className="form-control"
                     id="username"
                     value={credentials.username}
-                    onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+                    onChange={(e) =>
+                      setCredentials({
+                        ...credentials,
+                        username: e.target.value,
+                      })
+                    }
                   />
                 </div>
                 <div className="mb-3">
@@ -40,10 +50,19 @@ function Signin() {
                     className="form-control"
                     id="password"
                     value={credentials.password}
-                    onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+                    onChange={(e) =>
+                      setCredentials({
+                        ...credentials,
+                        password: e.target.value,
+                      })
+                    }
                   />
                 </div>
-                <button type="button" className="btn btn-primary w-100" onClick={signin}>
+                <button
+                  type="button"
+                  className="btn btn-primary w-100"
+                  onClick={signin}
+                >
                   Sign In
                 </button>
               </form>
